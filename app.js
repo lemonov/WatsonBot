ba24b6c72e833e582c5d5b06ae1d5a45db7382df
lemanpio <piotr.lemanczyk@brightone.pl>
2017-10-18 09:54:19 +0200

    Added dynamic content example

--- app.js
+++ app.js
@@ -30,12 +30,94 @@ router.get('/', function (req, res) {
             console.error(err);
         } else {
             console.log(response);
-            res.json({ message: response.output.text[0] });
+            response = handleResponse(response)
+            res.json({ message: response });
         }
     });
 });
 
 app.listen(port);
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getNextPhoneCall(type) {
+    var premessage = ""
+    if ("outgoing" != type && "incoming" != type) {
+        if (getRndInteger(0, 9) % 2 === 0) {
+            premessage = 'incoming call from: ';
+        }
+        else {
+            premessage = 'outgoing call to: ';
+        }
+    }
+    return premessage + getRndInteger(555000000, 580999999) + '\n';
+}
+
+function getPhoneCalls(numberOfQueries, type) {
+    var output = '';
+    for (var i = 0; i < numberOfQueries; i++) {
+        output += getNextPhoneCall(type);
+    }
+    return output;
+}
+
+function lastCalls(text, params) {
+    console.log("lastCalls");
+    var type = "";
+    var count = 0;
+    params.forEach(function (element) {
+        console.log(element.entity + " : " + element.value);
+
+        switch (element.entity) {
+            case 'call_type':
+                type = element.value;
+                break;
+            case 'sys-number':
+                count = element.value;
+                break;
+        }
+    });
+    console.log(type)
+    console.log(count)
+
+    calls = getPhoneCalls(count, type)
+    console.log(calls)
+    text = text.replace("^user_calls", calls)
+    console.log("RESPONSE: " + text)
+    return text
+}
+
+
+function accountBalance(text, params) {
+    return text;
+}
+
+
+function handleResponse(response) {
+    console.log("handleResponse");
+    var intent = response.intents[0].intent;
+    var text = response.output.text[0];
+    var params = response.entities;
+    var result = text;
+    console.log("Intent: " + intent);
+    console.log("Text: " + text);
+    console.log("Params: " + params);
+
+    switch (intent) {
+        case "last_calls":
+            result = lastCalls(text, params);
+            break;
+        case "account_balance":
+            console.log("CASE: last_calls")
+            result = accountBalance(text, params);
+            break;
+    }
+
+    console.log(result)
+    return result;
+}
 
 function sayIt(string) {
     // var text_to_speech = new TextToSpeechV1({
@@ -53,5 +135,3 @@ function sayIt(string) {
     // // Pipe the synthesized text to a file
     // text_to_speech.synthesize(params).pipe(fs.createWriteStream('buffer.wav'));
 }
-
-
